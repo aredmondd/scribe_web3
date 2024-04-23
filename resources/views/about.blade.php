1b03ee03d0dc317f7bd01f34c29c96db4893801d
“Aiden <adnrdmnd@gmail.com>
2024-04-23 10:03:51 -0400

    about page & tiny changes

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,21 @@
-<x-wrapper>
-    about
+<x-wrapper css="/css/about.css">
+    <div class="intro"><h1>HOW TO USE SCRIBE</h1></div>
+    <div class="body">
+        <br>
+        <br>
+        <br>
+        <p>1. Add a game using <a href="/add">"SCRIBE"</a> in the navbar.</p>
+        <br>
+        <p>2. Move games from your backlog to currently playing.</p>
+        <br>
+        <p>3. Once you are done playing, move it to it's respective category.</p>
+        <br>
+        <div class="def">
+            <p><span style="color: #E4572E">Backlog</span>: Want to play</p>
+            <p><span style="color: #F3A712">Currently Playing</span>: Are playing right now</p>
+            <p><span style="color: #A8C686">Dropped</span>: DNF. Won't try again</p>
+            <p><span style="color: #669BBC">Shelved</span>: DNF, might try again (or retiring an infinite game)</p>
+            <p><span style="color: #7189E0">Beat</span>: Games you have played to completion</p>
+        </div>
+    </div>
 </x-wrapper>
